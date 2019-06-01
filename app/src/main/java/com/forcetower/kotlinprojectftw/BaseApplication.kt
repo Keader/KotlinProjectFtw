930ab39d4263647793f1d27be4161c33131a58b7
Keader <keader.android@gmail.com>
2019-06-01 09:10:29 -0300

    Initial Commit :rocket:

---
+++ app/src/main/java/com/forcetower/kotlinprojectftw/BaseApplication.kt
@@ -0,0 +1,10 @@
+package com.forcetower.kotlinprojectftw
+
+import android.app.Application
+
+class BaseApplication : Application() {
+
+    override fun onCreate(){
+        super.onCreate();
+    }
+}
